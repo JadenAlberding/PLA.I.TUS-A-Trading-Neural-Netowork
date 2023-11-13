8ed2a7faf8829ad1faf92d35aee32d555e0f82ca
Jaden Alberding <jaden.alberding88@gmail.com>
2023-11-12 16:05:32 -0800

    Main function containg calls to FunctionsNNTut

--- NeuralNetworkTut.py
+++ NeuralNetworkTut.py
@@ -1,6 +1,14 @@
+import FunctionsNNTut as func
+
 
 def main():
-    print("Hello World")
+    input_vector = [1.72,1.23]
+    weight_1 = [1.26,0]
+    weight_2 = [2.17,0.32]
+    inputandW1 = func.dot_product(input_vector,weight_1)
+    inputandW2 = func.dot_product(input_vector,weight_2)
+    print(inputandW1,inputandW2)
+
 
 if __name__ == "__main__":
-    main();
+    main()
