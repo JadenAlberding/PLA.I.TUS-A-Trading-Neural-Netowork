21d1f7d80acd35121a926b60b2b71fb60c1fd4df
Jaden Alberding <jaden.alberding88@gmail.com>
2023-11-12 16:04:18 -0800

    Added sigmoid function

--- FunctionsNNTut.py
+++ FunctionsNNTut.py
@@ -1,8 +1,17 @@
+import numpy as np
+
 def dot_product(input1, input2):
+
+#    Computes the dot product of two given vectors
+#    of the same size. 
+
     arr = []
     for x,y in zip(input1,input2):
         arr.append(x*y)
     dot = 0
     for e in arr:
         dot+= e
-    return float(dot)
+    return '{:0.4f}'.format(dot)
+
+def sigmoid(x):
+    return 1/ (1+np.exp(-x))
