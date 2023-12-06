c80ab01eb9f2a8416c72fe7518e4cd9ed6a558bb
Jaden Alberding <jaden.alberding88@gmail.com>
2023-12-05 18:38:21 -0800

    Created the inital AI file

---
+++ PlAItus.py
@@ -0,0 +1,33 @@
+'''
+
+██████╗ ██╗       █████╗      ██╗  ████████╗██╗   ██╗ ██████╗
+██╔══██╗██║      ██╔══██╗     ██║  ╚══██╔══╝██║   ██║██╔════╝
+██████╔╝██║      ███████║     ██║     ██║   ██║   ██║╚█████╗
+██╔═══╝ ██║      ██╔══██║     ██║     ██║   ██║   ██║ ╚═══██
+██║     ███████╗ ██║  ██║ ██╗ ██║ ██╗ ██║   ╚██████╔╝██████╔╝
+╚═╝     ╚══════╝ ╚═╝  ╚═╝ ╚═╝ ╚═╝ ╚═╝ ╚═╝    ╚═════╝ ╚═════╝
+
+
+PlA.I.tus (play-tus) is a nerual network created to automate
+trading for stocks and crypto. The name PlA.I.tus was derived
+from the greek god Plutus, the god of wealth. 
+
+Developer: Jaden Alberding
+
+'''
+
+
+import numpy as np
+
+
+
+
+'''
+Add to README later vvvvv
+
+Information on creating the neural network was self taught.
+
+Source of information:
+    Samson Zhang, "Building a neural network FROM SCRATCH (no Tensorflow/Pytorch, just numpy & math)",https://www.youtube.com/watch?v=w8yWXqWQYmU&t=496s
+    Jason Brownlee "How to Choose an Activation Function for Deep Learning", https://machinelearningmastery.com/choose-an-activation-function-for-deep-learning/
+'''
